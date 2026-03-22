9b7ccbd5184080e5e7fea3157750131fc2d74216
chiennxhe181121 <chiennxhe181121@fpt.edu.vn>
2026-03-22 15:12:12 +0700

    add: view login, register

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SharingPictureWebsite.Controllers
+{
+    [Route("")]
+    public class AuthController : Controller
+    {
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
